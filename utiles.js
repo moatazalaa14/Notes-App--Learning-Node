460ef8c936839d08bbbac47272c5a90fb9449d10
Mu'taz <mutazalaasalah@gmail.com>
2021-01-08 18:15:51 +0200

    Adding a note

--- utiles.js
+++ utiles.js
@@ -1,5 +1,24 @@
-
+const fs=require("fs")
 const getNotes =function(notes){
     console.log(notes)
 }
-module.exports=getNotes;
+
+const addNote=function(title,body){
+    const notes= loadNotes()
+    console.log(notes.name)
+}
+const loadNotes=function(){
+    try {
+        const dataBuffer=fs.readFileSync("./notes.json")
+    const dataJson=JSON.parse(dataBuffer.toString())
+    return dataJson
+    } catch (error) {
+        return "ggg"
+    }
+    
+}
+
+module.exports={
+    getNotes:getNotes,
+    addNote:addNote
+}
