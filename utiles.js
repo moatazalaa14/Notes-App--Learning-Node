0a2a9470da654476cd3f46501ea9deb3e04fa7bc
Mu'taz <mutazalaasalah@gmail.com>
2021-01-12 00:53:31 +0200

    saving notes into json

--- utiles.js
+++ utiles.js
@@ -5,17 +5,33 @@ const getNotes =function(notes){
 
 const addNote=function(title,body){
     const notes= loadNotes()
-    console.log(notes.name)
+    const newNotes=notes.filter(function(note){
+        return note.name === title
+    })
+    if(newNotes.length===0){
+    notes.push(
+        {name:title,age:body}
+        )   
+        saveNotes(notes)
+        console.log("done")
+    }else{
+        console.error("title is used")
+    }
+
+}
+
+const saveNotes=(notes)=>{
+ const dataJson=JSON.stringify(notes)
+ fs.writeFileSync("notes.json",dataJson)
 }
 const loadNotes=function(){
     try {
         const dataBuffer=fs.readFileSync("./notes.json")
-    const dataJson=JSON.parse(dataBuffer.toString())
-    return dataJson
+        const dataJson=JSON.parse(dataBuffer.toString())
+        return dataJson
     } catch (error) {
         return "ggg"
     }
-    
 }
 
 module.exports={
